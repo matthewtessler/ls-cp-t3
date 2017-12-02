e9b5866480eaf02d35ce0258e34bf33978553d5d
matthewtessler <matthewtessler96@gmail.com>
2017-12-02 15:49:29 -0500

    express installed

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Tic-Tac-Toe' });
+});
+
+router.get('/game', function(req,res,next) {
+	res.render('game', {title:'Tic-Tac-Toe'});
+})
+
+module.exports = router;
