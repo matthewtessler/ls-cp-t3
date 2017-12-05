e381623540f2d2b039a3712195178541debf2766
Wazir Rafeek <war256@nyu.edu>
2017-12-04 19:58:38 -0500

    started adding client side script for OAuth

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
 var mysql = require('mysql');
 
-//set up user profile from google login
-function onSignIn(googleUser) { 
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+
 
 //Create the MySQL Connection
 var connect = function(){
